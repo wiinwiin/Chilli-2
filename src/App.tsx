a0505845da973870cd91b0b2df7b066d4e340f82
wiinwiin <wiinwiinlayar@gmail.com>
2026-04-01 16:06:36 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import HeartPage from "./pages/Heart";
+import Employers from "./pages/Employers";
+import { Layout } from "./components/layout/Layout";
+ 
+const queryClient = new QueryClient();
+ 
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Index />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/services/:type" element={<Services />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/heart" element={<HeartPage />} />
+            <Route path="/employers" element={<Employers />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+ 
+export default App;
+ 
